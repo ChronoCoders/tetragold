166bd1e36edf4bdfd3fdc99b23c16c95726ae18c
Altug Tatlisu <altug@bytus.io>
2025-12-16 19:04:32 +0000

    Fix: Import console from forge-std/console.sol
    The console library must be imported separately from forge-std/console.sol,
    not from Script.sol. This fixes the GitHub workflow failure.

--- script/DeployTGAUX.s.sol
+++ script/DeployTGAUX.s.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
 import {TGAUX} from "../src/TGAUX.sol";
 
 /**
